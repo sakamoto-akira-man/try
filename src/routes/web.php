ab2b0ac56edc6de90a6e8b3360da1064bd144326
root <root@DESKTOP-JHIQTAG.>
2025-01-15 10:31:12 +0900

    リモートリポジトリの変更

---
+++ src/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TryController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [TryController::class, 'index']);
+Route::middleware('auth')->group(function () {
+     Route::get('/', [TryController::class, 'index']);
+ });
